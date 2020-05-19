0d79b923e35d92a3a275594b969399a70c91df9a
Bruno Silva <brunos.souza2@gmail.com>
2020-05-19 18:34:35 -0300

    api

--- backend/app/controllers/api.py
+++ backend/app/controllers/api.py
@@ -1,27 +1,16 @@
 from flask import request, jsonify
-from app import app
-
-
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
+from app import app, db
+from app.models.models import Funcionario
 
 
 @app.route("/api/funcionarios", methods=['GET'])
 def api():
-    return jsonify(books)
+    funcionarios = Funcionario.query.all()
+
+    jsonData = []
+    for f in funcionarios:
+        cursos  = [{'id':x.id,'titulo':x.titulo,'descricao':x.descricao} for x in f.cursos]
+        jsonData.append({'nome':f.nome,'matricula':f.matricula, 'cursos': cursos})
+
+    return jsonify(jsonData)
 
